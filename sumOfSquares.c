16740e5ece767bed6e19ba7803a8b52df9f7652a
Hjalmar Rutberg <hjalmar.rutberg@gmail.com>
2026-05-06 10:51:39 +0200

    Sum of squares in C + gitignore for .o compiled C files

---
+++ sumOfSquares.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Short sum of squares program that reads in a list of integers from a file 
+// and calculates the sum of their squares
+
+void sumOfSquares(int *list, int listSize,long long *result);
+
+int main(int argc, char *argv[]) {
+    
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) { 
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    // Find size of input list
+    int listSize = 0;
+    int temp;
+    while (fscanf(fp, "%d", &temp) == 1) {
+        listSize++;
+    }
+
+    // Rewind filepointer and then read all numbers into an array
+    rewind(fp);
+    int *list = (int *)malloc(listSize * sizeof(int));
+    for (int i = 0; i < listSize; i++) {
+        fscanf(fp, "%d", &list[i]);
+    }
+
+    // Calculate sum of squares
+    long long *result = malloc(sizeof(long long));
+    sumOfSquares(list, listSize, result);
+    printf("Sum of squares: %lld\n", *result);
+
+    free(list);
+    free(result);
+    fclose(fp);
+    return 0;
+}
+
+void sumOfSquares(int *list, int listSize,long long *result) {
+    long long sum = 0;
+    for (int i = 0; i < listSize; i++) {
+        sum += (long long)list[i] * list[i];
+    }
+    *result = sum;
+}
